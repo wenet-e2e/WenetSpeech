9e15248078d9cc41462ced01f53ad481c6169cc3
Binbin Zhang <binbzha@qq.com>
2021-10-11 17:44:29 +0800

    rename data to metadata

--- utils/download.sh
+++ utils/download.sh
@@ -144,7 +144,7 @@ fi
 # Download from list
 if [ $stage -le 2 ]; then
   echo "$0: Start to download WenetSpeech files(*.aes.tgz)"
-  grep -v '^#' data/test.list | (while read line; do
+  grep -v '^#' metadata/test.list | (while read line; do
     download_object_from_release $line || exit 1;
   done) || exit 1;
 fi
@@ -152,7 +152,7 @@ fi
 # Process data
 if [ $stage -le 4 ]; then
   echo "$0: Start to process the downloaded files(*.aes.tgz)"
-  grep -v '^#' data/test.list | (while read line; do
+  grep -v '^#' metadata/test.list | (while read line; do
     process_downloaded_object $line || exit 1;
   done) || exit 1;
 fi
