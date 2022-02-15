acb816297ac4f2b640f4ae2cc5aaa6154199329e
Binbin Zhang <binbzha@qq.com>
2022-02-15 10:14:43 +0800

    update download url

--- utils/download_wenetspeech.sh
+++ utils/download_wenetspeech.sh
@@ -9,7 +9,7 @@ set -o pipefail
 
 stage=0
 
-WENETSPEECH_RELEASE_URL=https:/wenet-sh-1258344699.file.myqcloud.com/WenetSpeech/
+WENETSPEECH_RELEASE_URL=http://wenet.meeting.tencent.com/WenetSpeech/
 
 if [ $# -ne 2 ]; then
   echo "Usage: $0 <download-dir> <untar-dir>"
